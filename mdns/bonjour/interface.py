3953eeaa92a57505a9f11561df80de42d95610ad
D1plo1d <d1plo1d@d1plo1d.com>
2013-04-10 19:43:16 -0400

    Adding bonjour save_group implementation

--- mdns/bonjour/interface.py
+++ mdns/bonjour/interface.py
@@ -1,4 +1,10 @@
 import os
+import select
+import sys
+import pybonjour
+import atexit
+from pprint import pprint
+
 
 class Backend:
     def types(self):
@@ -7,8 +13,20 @@ class Backend:
     def load_group(self, alternate_location=None):
         print "NOT IMPLEMENTED"
     
-    def save_group(self, alternate_location=None):
-        print "NOT IMPLEMENTED"
+    def save_group(self, servicegroup, alternate_location=None):
+        name = servicegroup['name']
+        self.add_service(name, servicegroup['services'])
+
+    def add_service(self, name, opts):
+        sdRef = pybonjour.DNSServiceRegister(name = name,
+                                            regtype = opts['type'],
+                                            port = opts['port'],
+                                            domain = opts['domain'])
+        atexit.register(self.cleanup_service, sdRef)
+
+    def cleanup_service(self, sdRef):
+        sdRef.close()
+
 
     def remove_group(self, alternate_location=None):
         print "NOT IMPLEMENTED"
